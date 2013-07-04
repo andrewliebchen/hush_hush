a2ccc432951fd52de573749fc8ff2197ee319798
Andrew Liebchen <aliebchen@gofdn.com>
2013-07-04 19:04:59 -0400

    Add commit history

--- 0.0.2/javascripts/app.js
+++ 0.0.2/javascripts/app.js
@@ -1,14 +1,28 @@
 $(function(){
+  // Size the fillometers
   $('.fillometer').each(function(){
     var fillometerWidth = $(this).data('fillometer-width');
     $(this).children('.fillometer-fill').css('width', fillometerWidth);
   });
 
+  // Hide the header
   $('body.page-test').addClass('header-retracted');
+
   $('.header-toggle').mouseenter(function(){
     $('body.page-test').removeClass('header-retracted');
   });
+
   $('.header').mouseleave(function(){
     $('body.page-test').addClass('header-retracted');
+  });
+
+  // Add tooltips
+  // $('[data-tooltip]').each(function(){
+  //   var tooltipMessage = $(this).attr('data-tooltip');
+  //   $(this).append('<div class="tooltip">' + tooltipMessage + '</div>');
+  // });
+
+  $('[data-toggle="test-chooser"]').click(function(){
+    $('.test-chooser').toggleClass('is-active');
   });
 });
