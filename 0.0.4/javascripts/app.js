2fbf41a672b8662df349ff4653f6ba6a899ad069
Andrew Liebchen <aliebchen@gofdn.com>
2013-07-10 23:55:59 -0400

    Continue with main section and fillometer

--- 0.0.4/javascripts/app.js
+++ 0.0.4/javascripts/app.js
@@ -1,43 +1,8 @@
 $(function(){
   // Size the fillometers
-  $('.fillometer').each(function(){
-    var fillometerWidth = $(this).data('fillometer-width');
-    $(this).children('.fillometer-fill').css('width', fillometerWidth);
-  });
-
-  // Collapse scenario list
-  $('[data-collapse="scenarios"]').click(function(){
-    $(this).toggleClass('is-pressed');
-    $(this).parents('.test').toggleClass('is-collapsed');
-  });
-
-  // Hide the header
-  $('body.page-test').addClass('header-retracted');
-
-  $('.header-toggle').mouseenter(function(){
-    $('body.page-test').removeClass('header-retracted');
-  });
-
-  $('.header').mouseleave(function(){
-    $('body.page-test').addClass('header-retracted');
-  });
-
-  // Add tooltips
-  // $('[data-tooltip]').each(function(){
-  //   var tooltipMessage = $(this).attr('data-tooltip');
-  //   $(this).append('<div class="tooltip">' + tooltipMessage + '</div>');
-  // });
-
-  // Toggle between fillometer and commits
-  $('[data-toggle="test-chooser"]').click(function(){
-    $('.test-chooser').toggleClass('is-active');
-  });
-
-  // Toggle panes
-  $('[data-toggle-pane]').click(function(){
-    toggleId = $(this).data('toggle-pane');
-    $(this).toggleClass('is-pressed');
-    $('#' + toggleId).toggleClass('is-active');
+  $('[data-fillometer]').each(function(){
+    var fillometerWidth = $(this).data('fillometer');
+    $(this).css({'background' : '-webkit-gradient(to right, #3d3d3d ' + fillometerWidth + ', transparent ' + fillometerWidth});
   });
 
   // Preview slider
